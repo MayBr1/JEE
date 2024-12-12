b2a17f1c6641bca3c20ca2d52f9e5612035d89aa
May Braham <maybraham1@gmail.com>
2024-12-12 16:54:45 +0100

    init commit

---
+++ src/main/java/com/example/jeeproj/JeeprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeeproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jeeproj/JeeprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeeproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
